e66851a1b62441375adea62b48bb773e4521f2f4
SylarPower <31884262+SylarPower@users.noreply.github.com>
2026-09-10 17:01:52 +0200

    Update saas-config.js

--- js/saas-config.js
+++ js/saas-config.js
@@ -1,9 +1,9 @@
 // Configurazione pubblica, senza segreti. Attivare il SaaS solo dopo deploy di
 // Functions, Rules, indici e provisioning dei profili cliente.
 window.PIANO_SAAS_CONFIG = {
-  enabled: false,
+  enabled: true,
   shoppingRewardedAds: {
-    enabled: true,
+    enabled: false,
     provider: null,
     unlockHours: 24,
     consentVersion: null
